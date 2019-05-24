b53bb988e7607da28ec81dea8f3a4e582bb4bbde
Tayfun Yaltur <36271003+tayfunyaltur@users.noreply.github.com>
2019-05-24 17:31:33 +0300

    Summaries
    eklenen methodlara gereli aciklamalar ile summary ler eklendi

--- Otel Rezervasyon Sistemi/Otel Rezervasyon Sistemi/Controllers/AuthenticationController.cs
+++ Otel Rezervasyon Sistemi/Otel Rezervasyon Sistemi/Controllers/AuthenticationController.cs
@@ -9,6 +9,12 @@ namespace Otel_Rezervasyon_Sistemi.Controllers
 {
     class AuthenticationController
     {
+        ///<summary>
+        ///<para>Giris yapmak isteyen kullanicin id ve sifresini alir</para>
+        /// <returns>uygun Musteri ya da Yonetici nesnelerinin kopyasini dondurur , duruma gore hata dondurur</returns>
+        /// </summary>
+        /// <param name="id">kullanicinin girdigi ID </param>
+        /// <param name="password">kullanicinin girdigi sifre </param>
         public static Musteri AccountVerification (string id , string password)
         {
             /*giris yapmak istenen kullanicinin var olup olmadigi kontrolu yapilacak oyle bir musteri varsa ;
@@ -17,12 +23,26 @@ namespace Otel_Rezervasyon_Sistemi.Controllers
              */
             return new Musteri("", "", "");
         }
+
+        /// <summary>
+        /// <para>uretilmek istenen hesap biliglerini alicak</para>
+        /// <returns>Kullanici yaratilirsa dogru yaratilamazsa false dondurulucek</returns>
+        /// </summary>
+        /// <param name="A" > gecici olarak olusturulmus olan kullanici nesnesi</param>
         public static bool CreateAccountRequest(Kullanici A )
         {
             /*kayit edilmek istenen kullanici durumlara uygun ise db ye eklenicek (eklenirse true) ,
              uygun degilse false donucek*/
             return false;
         }
+
+
+        ///<summary>
+        ///herhangi bir otele eklenmek istenen oda bilgileri alinicak
+        ///<returns>Oda Eklenirse true , hata olusursa hatayi aciklayan bir mesaj ile hata donucek</returns>
+        ///</summary>
+        ///<param name="A">gecici olarak olusturulmus oda nesnesi</param>
+        ///<param name="B">islemi yapmak isteyen kullanici nesnesini alir</param>
         public static bool AddRoomRequest(Oda A,Kullanici B)
         {
             /* eklenmek istenen oda daha once eklendiyse hata dondurucek, 
@@ -32,7 +52,14 @@ namespace Otel_Rezervasyon_Sistemi.Controllers
              */
             return false;
         }
-        public static bool ChangePasswordRequest(Kullanici B , string pas,string pasVerification)
+        /// <summary>
+        /// Kullanicinin sifre degistirme istegi
+        /// </summary>
+        /// <param name="B">Sifreyi degistirmek isteyen kullanici nesnesi</param>
+        /// <param name="newPas">Kullanicinin belirledigi yeni sifre</param>
+        /// <param name="newPasVerification">Kullanicinin belirledigi yeni sifrenin dogrulamasi</param>
+        /// <returns>islem uygun ise True , degil ise hata dondurur</returns>
+        public static bool ChangePasswordRequest(Kullanici B , string newPas,string newPasVerification)
         {
             /*
              * sifresini degistirmek isteyen kullanicinin bilgileri ve yeni yapmak istedigi 
@@ -41,6 +68,13 @@ namespace Otel_Rezervasyon_Sistemi.Controllers
              */
             return false;
         }
+        /// <summary>
+        /// kullanicinin bilgi guncelleme istegi 
+        /// </summary>
+        /// <param name="B">Bilgisi guncellenicek olan kullanici nesnesi</param>
+        /// <param name="whichInfo">Hangi bilinin guncellenecegi</param>
+        /// <param name="newInfo">Guncellenen bilginin yeni hali</param>
+        /// <returns>islem uygulanir ise true , hata olusur ise hata dondurucekti</returns>
         public static bool ChangeInformationRequest(Kullanici B , string whichInfo , string newInfo)
         {
             /*
@@ -49,6 +83,13 @@ namespace Otel_Rezervasyon_Sistemi.Controllers
 
             return false;
         }
+        /// <summary>
+        /// Otele Ait bir odayi silme islemi
+        /// </summary>
+        /// <param name="B">Silmek isteyen kullanici nesesi</param>
+        /// <param name="odano">Silinmek istenen oda numarasi</param>
+        /// <param name="otelid">Odanin ait oldugu otel</param>
+        /// <returns>Islem gerceklesirse true ; hata olusur ise hata doner</returns>
         public static bool DeleteRoomRequest(Kullanici B , int odano , int otelid)
         {
             /*
@@ -59,6 +100,7 @@ namespace Otel_Rezervasyon_Sistemi.Controllers
              */
             return false;
         }
+        
         /*
          * ihtiyaca bagli olarak dahasi da eklenecektir .
          */
---
+++ Otel Rezervasyon Sistemi/Otel Rezervasyon Sistemi/Controllers/ViewControllers.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Otel_Rezervasyon_Sistemi;
+
+namespace Otel_Rezervasyon_Sistemi.Controllers
+{
+    class ViewControllers
+    {
+        /// <summary>
+        /// Otel Odalari listelenmek istediginde cagirilmali
+        /// </summary>
+        /// <param name="otelId">odalari listelenicek otelin idsi</param>
+        /// <returns> List<Oda>  dondurur </returns>
+        public static List<Oda> GetRooms(string otelId)
+        {
+            /*
+             * herhangi bir otelin odalari gosterilmek istendiginde cagirilmasi gereken method 
+             */
+            return new List<Oda>();
+        }
+        /// <summary>
+        /// Oda bilgilerini goruntulemek icin cagirilir
+        /// </summary>
+        /// <param name="odano">bilgileri goruntulenicek oda no</param>
+        /// <param name="otelid">bilgileri goruntulenicek odanin ait oldugu otel idsi</param>
+        public static void GetRoomDetails(int odano, string otelid)
+        {
+            /*
+             * Dondurcegi sey oda fieldlari tamamlaninca belirlenicek sadece taslak icin void olarak belirlendi
+             * 
+             */
+        }
+        /// <summary>
+        /// Odanin rezervasyon bilgileri istendiginde dondurur
+        /// </summary>
+        /// <param name="odano">rezervasyon bilgileri istenen odanin numarasi</param>
+        /// <param name="otelid">rezervasyon bilgileri istenen odanin ait oldugu otel id </param>
+        /// <returns></returns>
+        public static void GetReservations(int odano, string otelid)
+        {
+            /*
+             * odanin rezervasyonlari dondurucek method
+             */
+        }
+        /// <summary>
+        /// Otelin butun rezervasyon bilgilerini dondurur
+        /// </summary>
+        /// <param name="otelid">Rezervasyonu sorgulanmak istenen otelin idsi</param>
+        public static void GetReservations(string otelid)
+        {
+            /*
+             * otelin butun rezervasyonlarini dondurucek olan id 
+             */
+        }
+    }
+}
