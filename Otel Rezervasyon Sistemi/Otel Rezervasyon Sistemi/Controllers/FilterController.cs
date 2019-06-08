b586223a6c487e7803ea30f1aab2c3849c30632d
Tayfun Yaltur <36271003+tayfunyaltur@users.noreply.github.com>
2019-06-09 00:01:14 +0300

    FilterController.HotelFilter wrote for ALLfilters

--- Otel Rezervasyon Sistemi/Otel Rezervasyon Sistemi/Controllers/FilterController.cs
+++ Otel Rezervasyon Sistemi/Otel Rezervasyon Sistemi/Controllers/FilterController.cs
@@ -13,94 +13,111 @@ namespace Otel_Rezervasyon_Sistemi.Controllers
         {
 
         }
+        private Core core = new Core();
         /// <summary>
-        /// Oda filtreleme icin cagirilmasi gereken method
+        /// butun filtreler uygulandiginda cagirilicak method Tam olarak istenileni donduru eger TV secilmemis ise icerisinde TV olmayan ve diger kosullara uygun olanlari dondurur
         /// </summary>
-        /// <param name="hotelType">Otel tipi string ["Butik" , "TatilKoyu" , "Pansiyon"]</param>
-        /// <param name="roomType">Oda tipi string ["Standart" , "Manzarali" , "KralDairesi"]</param>
-        /// <param name="minPoint">int alt puan</param>
-        /// <param name="minPrice">int alt fiyat</param>
-        /// <param name="maxPrice">int ust fiyat</param>
-        /// <param name="start">datetime baslangic tarihi</param>
-        /// <param name="finish">date time bitis tarihi</param>
-        /// <param name="checks">oda Ozellikleri </param>
-        /// <returns></returns>
-        public List<Oda> RoomFilter(string hotelType , string roomType , int minPoint , int minPrice , int maxPrice , DateTime start , DateTime finish , bool[] checks)
+        /// <param name="hotelType">Otel tipini dogrudan comboboxdaki gibi gonder</param>
+        /// <param name="roomType">Oda tipini dogrudan comboboxdaki gibi gonder</param>
+        /// <param name="point">odada aranan minimum yildiz puani</param>
+        /// <param name="minPrice">odanin mininmum fiyati</param>
+        /// <param name="maxPrice">odanin maximum fiyati</param>
+        /// <param name="wifi"></param>
+        /// <param name="minibar"></param>
+        /// <param name="klima"></param>
+        /// <param name="televizyon"></param>
+        /// <param name="start">baslangic tarihi</param>
+        /// <param name="end">bitis tarihi</param>
+        /// <returns>"OtelID-OtelAdi-OdaNumarasi" formatinda elemanlari olan bir string listesi dondurur</returns>
+        public List<string> HotelFilter(string hotelType, string roomType, int point, int minPrice, int maxPrice, bool wifi, bool minibar, bool klima, bool televizyon, DateTime start, DateTime end)
         {
-            List<Otel> hotels = new List<Otel>();
-            if(hotelType == "Butik")
+            /*!!!CORE!!!otel tipine gore otel idlerini ve isimlerini donduren method  orn.("OtelID-OtelADI-Yildizi")*/
+            List<string> hotelIDandNamesandStars = new List<string>();
+            List<string> hotelIDs = new List<string>();
+            List<string> hotelNames = new List<string>();
+            List<string> hotelStars = new List<string>();
+            foreach (string s in hotelIDandNamesandStars)
             {
-                //get Butiks from core
-            }else if(hotelType == "TatilKoyu")
+                hotelIDs.Add(s.Split('-')[0]);
+                hotelNames.Add(s.Split('-')[1]);
+                hotelStars.Add(s.Split('-')[2]);
+            }
+            List<string> roomie = new List<string>();
+            List<List<string>> roomsOfHotel = new List<List<string>>();
+            for (int i = 0; i < hotelIDs.Count; i++)
             {
-                //get TatilKoyu from core
-            }else if(hotelType == "Pansiyon")
+                if (Convert.ToInt32(hotelStars[i]) >= point)
+                {
+
+                    List<Oda> rooms = core.ReturnRoomObjects(hotelIDs[i]);
+
+                    foreach (Oda o in rooms)
+                    {
+                        bool available = true;
+                        if (roomType == "Kral Dairesi" && o is KralDairesi)
+                        {
+                            if (o.Klimali == klima && o.Televizyonlu == televizyon && o.Minibarli == minibar && o.Wifili == wifi && o.OdaFiyati > minPrice && o.OdaFiyati < maxPrice)
+                            {
+                                foreach (Rezervasyon r in o.Rezervasyonlar)
+                                {
+                                    if ((r.RezBaslangic < start && r.RezBitis > start) || (r.RezBaslangic < end && r.RezBitis > end))
+                                    {
+                                        available = false;
+                                    }
+                                }
+                                if (available)
+                                {
+                                    roomie.Add(hotelIDs[i] + "-" + hotelNames[i] + "-" + o.OdaNo.ToString());
+                                }
+                            }
+                        }
+                        else if (roomType == "Manzaralı" && o is ManzaraliOda)
+                        {
+                            if (o.Klimali == klima && o.Televizyonlu == televizyon && o.Minibarli == minibar && o.Wifili == wifi && o.OdaFiyati > minPrice && o.OdaFiyati < maxPrice)
+                            {
+                                foreach (Rezervasyon r in o.Rezervasyonlar)
+                                {
+                                    if ((r.RezBaslangic < start && r.RezBitis > start) || (r.RezBaslangic < end && r.RezBitis > end))
+                                    {
+                                        available = false;
+                                    }
+                                }
+                                if (available)
+                                {
+                                    roomie.Add(hotelIDs[i] + "-" + hotelNames[i] + "-" + o.OdaNo.ToString());
+                                }
+                            }
+                        }
+                        else if (roomType == "Standart" && o is StandartOda)
+                        {
+                            if (o.Klimali == klima && o.Televizyonlu == televizyon && o.Minibarli == minibar && o.Wifili == wifi && o.OdaFiyati > minPrice && o.OdaFiyati < maxPrice)
+                            {
+                                foreach (Rezervasyon r in o.Rezervasyonlar)
+                                {
+                                    if ((r.RezBaslangic < start && r.RezBitis > start) || (r.RezBaslangic < end && r.RezBitis > end))
+                                    {
+                                        available = false;
+                                    }
+                                }
+                                if (available)
+                                {
+                                    roomie.Add(hotelIDs[i] + "-" + hotelNames[i] + "-" + o.OdaNo.ToString());
+                                }
+                            }
+                        }
+                    }
+
+
+                }
+            }
+            if (roomie.Count > 0)
             {
-                //get pansiyon from core
+                return roomie;
             }
             else
             {
-                throw new Exception("Bu tipte bir otel bulunmamaktadir");
+                throw new Exception("Bu filtreye uygun bos oda bulunamiyor");
             }
-            List<Oda> _temp = new List<Oda>();
-            if(roomType == "Manzarali")
-            {
-                foreach(Otel o in hotels)
-                {
-                    foreach(Oda _o in o.Odalar )
-                    {
-                        if(o.Hizmet+o.Konum+o.Temizlik >= minPoint)
-                        {
-                            if (_o is ManzaraliOda && _o.OdaFiyati > minPrice && _o.OdaFiyati < maxPrice/*rezervasyon kontrolleri eklenicek*/)
-                            {
-                                _temp.Add(_o);
-                            }
-                        }
-                        
-                    }
-                }
-            }
-            else if(roomType == "Standart")
-            {
-
-                foreach (Otel o in hotels)
-                {
-                    foreach (Oda _o in o.Odalar)
-                    {
-                        if (o.Hizmet + o.Konum + o.Temizlik >= minPoint)
-                        {
-                            if (_o is StandartOda && _o.OdaFiyati > minPrice && _o.OdaFiyati < maxPrice/*rezervasyon kontrolleri eklenicek*/)
-                            {
-                                _temp.Add(_o);
-                            }
-                        }
-
-                    }
-                }
-            }
-            else if (roomType == "KralDairesi")
-            {
-                foreach (Otel o in hotels)
-                {
-                    foreach (Oda _o in o.Odalar)
-                    {
-                        if (o.Hizmet + o.Konum + o.Temizlik >= minPoint)
-                        {
-                            if (_o is KralDairesi && _o.OdaFiyati > minPrice && _o.OdaFiyati < maxPrice/*rezervasyon kontrolleri eklenicek*/)
-                            {
-                                _temp.Add(_o);
-                            }
-                        }
-
-                    }
-                }
-            }
-            else
-            {
-                throw new Exception("Boyle bir oda tipi bulunamadi");
-            }
-            return _temp;
-
         }
     }
 }
