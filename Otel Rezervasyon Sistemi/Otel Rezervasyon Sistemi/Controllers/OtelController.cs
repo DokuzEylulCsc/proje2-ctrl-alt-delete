8949b3bb7573e5223575e4eab5ddc7699aa5590e
Tayfun Yaltur <36271003+tayfunyaltur@users.noreply.github.com>
2019-06-08 13:49:06 +0300

    summaries fixed

--- Otel Rezervasyon Sistemi/Otel Rezervasyon Sistemi/Controllers/OtelController.cs
+++ Otel Rezervasyon Sistemi/Otel Rezervasyon Sistemi/Controllers/OtelController.cs
@@ -23,13 +23,16 @@ namespace Otel_Rezervasyon_Sistemi.Controllers
              * otelin butun rezervasyonlarini dondurucek olan id 
              */
         }
-        /// <summary>
-        /// genel listeye otel ekler 
-        /// </summary>
-        /// <param name="otelid">eklenecek olan otelin idsi</param>
-        /// <param name="otelname">eklenicek olan otelin ismi</param>
-        /// <param name="oteltype">eklenicek olan otelin tipi </param>
-        public  void AddHotel(string otelid , string otelname, string oteltype)
+       /// <summary>
+       /// Otel Ekleme Fonksiyonu
+       /// </summary>
+       /// <param name="otelid">Eklenicek olan otelin idsi</param>
+       /// <param name="otelname">Eklenicez olan otelin ismi</param>
+       /// <param name="oteltype">eklenicek olan otelin tipi {Pansiyon,Tatil,Butik}</param>
+       /// <param name="temizlik">temizlik puani</param>
+       /// <param name="konum">konum puani</param>
+       /// <param name="hizmet">hizmet puani</param>
+        public  void AddHotel(string otelid , string otelname, string oteltype,int temizlik ,int konum , int hizmet)
         {
             /*
              * gerekli implementasyon yapilacaka duruma gore parametre eklenip cikartilacak 
